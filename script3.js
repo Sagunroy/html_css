eb589537e13175f802636f3ac1e5aa7d9eab5ddf
SagunRoy <sagun09roy@gmail.com>
2024-01-24 21:12:33 +0530

    added

---
+++ script3.js
@@ -0,0 +1,16 @@
+
+
+// Base number input 
+let n = 5 
+  
+// Power input 
+let power = 3 
+  
+// Result variable 
+let num = 1; 
+for (let i = 0; i < power; ++i) { 
+    num = num * n; 
+} 
+  
+// Display output 
+console.log(num);
