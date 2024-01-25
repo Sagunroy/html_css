0c0b4217d82b0061da3bde72de4abb4558da658e
SagunRoy <sagun09roy@gmail.com>
2024-01-25 15:02:01 +0530

    add file

---
+++ calculator_script5.js
@@ -0,0 +1,32 @@
+let num1,num2
+
+
+function add(num1,num2) {
+    return num1 + num2;
+}
+
+function sub(num1,num2){
+return num2-num1;
+}
+
+function mul(num1,num2) {
+return num1 * num2;
+}
+
+function div(num1,num2) {
+  return num2 / num1;
+}
+
+function funky(num1,num2,num3) {
+    return (num2 * num3) + num1;
+}
+let num3 = add(2,4)
+console.log(num3)
+let num4 = sub(8,4)
+console.log(num4)
+let num5 = mul(2,4)
+console.log(num5)
+let num6 = div(2,4)
+console.log(num6)
+let num7 = funky(2,4,3)
+console.log(num7)
---
+++ fun_script4.js
@@ -0,0 +1,29 @@
+//functions
+let myvar=9
+function numberss()
+{
+    console.log(myvar)
+}
+numberss()
+
+//function with parameters
+let myvar1=7
+function numbersss(increamtby,multiplyby)
+{
+    myvar1= myvar1 + increamtby * multiplyby
+}
+numbersss(2,3)
+console.log(myvar1)
+
+//function with return(after return no statement gets executed)
+const myvarr=5
+function numberr(increamtbyy,multiplybyy)
+{
+    return(myvarr + increamtbyy * multiplybyy)
+}
+const myvarrr = numberr(3,4) //return value stored in myvarrr
+console.log(myvarrr)
+
+
+
+
